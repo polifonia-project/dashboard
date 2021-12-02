4db008ae5143d3ff9c3198e452058dbfa920a22e
giuliarenda <giuliarenda94@gmail.com>
2021-12-02 15:56:18 +0100

    erased useless comments

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,3 @@
-// window.onload = function () {
-//     var sparqlEndpoint = data.sparql_endpoint;
-//     var sparqlQuery = data.count1;
-//     var encoded = encodeURIComponent(sparqlQuery);
-//     $.ajax({
-//         type: 'GET',
-//         url: sparqlEndpoint + '?query=' + encoded,
-//         headers: { Accept: 'application/sparql-results+json; charset=utf-8'},
-//         success: function (returnedJson) {
-//             for (i = 0; i < returnedJson.results.bindings.length; i++) {
-//                 var count = returnedJson.results.bindings[i].count.value;
-//                 var container = document.getElementById("first");
-//                 var div = document.createElement("div");
-//                 div.className = "done";
-//                 div.appendChild(document.createTextNode(count));
-//                 container.appendChild(div);
-//             }
-//         }
-//     })
-// }
-
 window.onload = function () {
     var sparqlEndpoint = data.sparql_endpoint;
     var counts = data.count;
