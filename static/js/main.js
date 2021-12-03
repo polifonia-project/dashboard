c9099c6edae40a7b9597554170f3ea20e24e1a36
mondoboia <giuliarenda94@gmail.com>
2021-12-03 10:56:16 +0100

    added labels to countings and layout improvement #5

--- static/js/main.js
+++ static/js/main.js
@@ -4,20 +4,31 @@ window.onload = function () {
 
     counts.forEach(element => {
         var sparqlQuery = element.query;
-        console.log(sparqlQuery);
         var encoded = encodeURIComponent(sparqlQuery);
+        var label = element.label;
         $.ajax({
             type: 'GET',
             url: sparqlEndpoint + '?query=' + encoded,
             headers: { Accept: 'application/sparql-results+json; charset=utf-8' },
             success: function (returnedJson) {
-                for (j = 0; j < returnedJson.results.bindings.length; j++) {
-                    var count = returnedJson.results.bindings[j].count.value;
-                    var container = document.getElementById("first");
-                    var div = document.createElement("div");
-                    div.className = "done";
-                    div.appendChild(document.createTextNode(count));
-                    container.appendChild(div);
+                for (i = 0; i < returnedJson.results.bindings.length; i++) {
+                    var count = returnedJson.results.bindings[i].count.value;
+                    // create div to contain number and label
+                    var generalDiv = document.createElement("div");
+                    generalDiv.className = "col-lg-4";
+
+                    var countDiv = document.createElement("div");
+                    countDiv.className = "card-body option-2b";
+                    countDiv.appendChild(document.createTextNode(count));
+
+                    var labelP = document.createElement("p");
+                    labelP.appendChild(document.createTextNode(label));
+                    countDiv.appendChild(labelP);
+
+                    generalDiv.appendChild(countDiv);
+                    // get container and append
+                    var container = document.getElementById("count_container");
+                    container.appendChild(generalDiv);
                 }
             }
         })
