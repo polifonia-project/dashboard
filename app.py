6573da4591d7356ca9567c21adb7d38fdd7a12b4
mondoboia <giuliarenda94@gmail.com>
2021-12-03 11:37:36 +0100

    addded check in case data_source in config has no count

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 import json
 
@@ -19,7 +19,6 @@ def access_data_sources(pilot_name):
         if pilot_name == source:
             return details
 
-
 @app.route("/<string:page_name>")
 def index(page_name):
     details = access_data_sources(page_name)
@@ -31,7 +30,7 @@ def index(page_name):
         desc = True
         return render_template('index.html', details=details, title=title, sub=sub, curator=curator, desc=desc)
     elif template == 'alt':
-        return render_template('ui-maps.html')
+        return render_template('ui-maps.html', details=details)
     else:
         return render_template('page-404.html')
 
@@ -39,27 +38,4 @@ def index(page_name):
 # @app.route("/<string:page_name>")
 # def index(page_name):
 #     return render_template('index.html', title="page", jsonfile=json.dumps(access_data_sources(page_name)))
-
-
-##  SPARQL QUERY
-#
-# ssl._create_default_https_context = ssl._create_unverified_context
-# sparql_endpoint = 'https://query.wikidata.org/'
-#
-# count_01_query = """
-# SELECT (COUNT(*) AS ?count)
-# WHERE {
-#   ?item wdt:P31 wd:Q5 .
-# }
-# """
-#
-# # set the endpoint
-# sparql_wd = SPARQLWrapper(sparql_endpoint)
-# # set the query
-# sparql_wd.setQuery(count_01_query)
-# # set the returned format
-# sparql_wd.setReturnFormat(JSON)
-# # get the results
-# results = sparql_wd.query().convert()
-
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,37 +1,38 @@
 window.onload = function () {
     var sparqlEndpoint = data.sparql_endpoint;
-    var counts = data.count;
+    if(data.count) {
+        data.count.forEach(element => {
+            var sparqlQuery = element.query;
+            var encoded = encodeURIComponent(sparqlQuery);
+            var label = element.label;/**/
+            $.ajax({
+                type: 'GET',
+                url: sparqlEndpoint + '?query=' + encoded,
+                headers: {Accept: 'application/sparql-results+json; charset=utf-8'},
+                success: function (returnedJson) {
+                    for (i = 0; i < returnedJson.results.bindings.length; i++) {
+                        var count = returnedJson.results.bindings[i].count.value;
+                        // create div to set the column
+                        var generalDiv = document.createElement("div");
+                        generalDiv.className = "col-lg-4";
+                        // create div to contain number and label
+                        var countDiv = document.createElement("div");
+                        countDiv.className = "card-body option-2b";
+                        countDiv.appendChild(document.createTextNode(count));
+                        generalDiv.appendChild(countDiv);
+                        // create and append p for label
+                        var labelP = document.createElement("p");
+                        labelP.appendChild(document.createTextNode(label));
+                        countDiv.appendChild(labelP);
 
-    counts.forEach(element => {
-        var sparqlQuery = element.query;
-        var encoded = encodeURIComponent(sparqlQuery);
-        var label = element.label;
-        $.ajax({
-            type: 'GET',
-            url: sparqlEndpoint + '?query=' + encoded,
-            headers: { Accept: 'application/sparql-results+json; charset=utf-8' },
-            success: function (returnedJson) {
-                for (i = 0; i < returnedJson.results.bindings.length; i++) {
-                    var count = returnedJson.results.bindings[i].count.value;
-                    // create div to contain number and label
-                    var generalDiv = document.createElement("div");
-                    generalDiv.className = "col-lg-4";
-
-                    var countDiv = document.createElement("div");
-                    countDiv.className = "card-body option-2b";
-                    countDiv.appendChild(document.createTextNode(count));
-
-                    var labelP = document.createElement("p");
-                    labelP.appendChild(document.createTextNode(label));
-                    countDiv.appendChild(labelP);
-
-                    generalDiv.appendChild(countDiv);
-                    // get container and append
-                    var container = document.getElementById("count_container");
-                    container.appendChild(generalDiv);
+                        // get container and append
+                        var container = document.getElementById("count_container");
+                        container.appendChild(generalDiv);
+                    }
                 }
-            }
+            })
         })
-    })
-
+    } else {
+        alert("No COUNT found.");
+    }
 }
