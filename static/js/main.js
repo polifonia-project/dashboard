f3c1fc04c3cb418ce04a526f4b78bd96baf8c11f
giuliarenda <giuliarenda94@gmail.com>
2021-12-16 12:28:45 +0100

    performance improvement barchart function #7

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     counter();
     chartViz();
+    // sidebarContent();
 }
 
 function counter() {
@@ -71,37 +72,7 @@ function chartViz() {
 }
 
 function barchart(element, index) {
-    // first create the HTML structure that'll receive the data
 
-    // create canva for bar chart
-    var chartCanva = document.createElement("canvas");
-    var chartId = "chart" + (index + 1);
-    chartCanva.setAttribute("id", chartId);
-
-    // create div that contains canva
-    var chartArea = document.createElement("div");
-    chartArea.className = "chart-area";
-    chartArea.appendChild(chartCanva);
-
-    // create card body div
-    var cardBody = document.createElement("div");
-    cardBody.className = "card-body";
-    cardBody.appendChild(chartArea);
-
-    // create chart title h3 and add data.label as text
-    var chartTitle = document.createElement("h3");
-    chartTitle.className = "card-title";
-    chartTitle.appendChild(document.createTextNode(element.label));
-
-    // create card header
-    var cardHeader = document.createElement("div");
-    cardHeader.className = "card-header";
-    cardHeader.appendChild(chartTitle);
-
-    // get general container and append elements
-    var container = document.getElementById("vizContainer");
-    container.appendChild(cardHeader);
-    container.appendChild(cardBody);
 
     // get the data that I need
     // now starts a piece of code that is exactly the same from function counter
@@ -133,40 +104,94 @@ function barchart(element, index) {
             url: sparqlEndpoint + '?query=' + encoded,
             headers: { Accept: 'application/sparql-results+json; charset=utf-8' },
             success: function (returnedJson) {
+                // first create the HTML structure that'll receive the data
+
+                // create canva for bar chart
+                var chartCanva = document.createElement("canvas");
+                var chartId = "chart" + (index + 1);
+                chartCanva.setAttribute("id", chartId);
+
+                // create div that contains canva
+                var chartArea = document.createElement("div");
+                chartArea.className = "chart-area";
+                chartArea.appendChild(chartCanva);
+
+                // create card body div
+                var cardBody = document.createElement("div");
+                cardBody.className = "card-body";
+                cardBody.appendChild(chartArea);
+
+                // create chart title h3 and add data.label as text
+                var chartTitle = document.createElement("h3");
+                chartTitle.className = "card-title";
+                chartTitle.appendChild(document.createTextNode(element.label));
+
+                // create card header
+                var cardHeader = document.createElement("div");
+                cardHeader.className = "card-header";
+                cardHeader.appendChild(chartTitle);
+
+                // get general container and append elements
+                var container = document.getElementById("vizContainer");
+                container.appendChild(cardHeader);
+                container.appendChild(cardBody);
+
                 for (i = 0; i < returnedJson.results.bindings.length; i++) {
                     chartLabels[i] = returnedJson.results.bindings[i].x.value;
                     chartData[i] = returnedJson.results.bindings[i].y.value;
-                }
+                } var ctx = document.getElementById(chartId).getContext("2d");
+
+                var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+
+                gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
+                gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
+                gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
+
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    responsive: true,
+                    legend: {
+                        display: false
+                    },
+                    data: {
+                        labels: chartLabels,
+                        datasets: [{
+                            fill: true,
+                            backgroundColor: gradientStroke,
+                            hoverBackgroundColor: gradientStroke,
+                            borderColor: '#1f8ef1',
+                            borderWidth: 2,
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            data: chartData,
+                        }]
+                    },
+                    options: gradientBarChartConfiguration
+                });
             }
         })
     }
-    var ctx = document.getElementById(chartId).getContext("2d");
 
-    var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
-
-    gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
-    gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
-    gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
-
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        responsive: true,
-        legend: {
-            display: false
-        },
-        data: {
-            labels: chartLabels,
-            datasets: [{
-                fill: true,
-                backgroundColor: gradientStroke,
-                hoverBackgroundColor: gradientStroke,
-                borderColor: '#1f8ef1',
-                borderWidth: 2,
-                borderDash: [],
-                borderDashOffset: 0.0,
-                data: chartData,
-            }]
-        },
-        options: gradientBarChartConfiguration
-    });
 }
+
+// function sidebarContent() {
+//     // general container
+//     var container = document.getElementById("sidebarWrapper");
+
+//     // project logo
+//     if (data.logo_path.length > 0) {
+//         var logoDiv = document.createElement("div");
+//         logoDiv.className("logo");
+
+//         var logoLink = document.createElement("a");
+//         logoLink.setAttribute("target", "_blank");
+//         logoLink.setAttribute("href", "data.iri_base");
+
+//         var logoImg = document.createElement("img");
+//         logoImg.setAttribute("src", data.logo_path);
+
+//         logoLink.appendChild(logoImg);
+//         logoDiv.appendChild(logoLink);
+//         container.appendChild(generalDiv);
+//     }
+// }
