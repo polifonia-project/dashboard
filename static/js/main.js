cf014bf145fe0aa37fcca5cc505e1fdaa10b87a7
giuliarenda <giuliarenda94@gmail.com>
2021-12-17 01:57:41 +0100

    linechart visualization #9

--- static/js/main.js
+++ static/js/main.js
@@ -63,8 +63,8 @@ function chartViz() {
             var chart = element.chart_type;
             if (chart === "barchart") {
                 barchart(element, index);
-            } else if (chart === "piechart") {
-                alert("Pie chart!");
+            } else if (chart === "linechart") {
+                linechart(element, index);
             }
         }
         )
@@ -141,12 +141,13 @@ function barchart(element, index) {
                     chartData[i] = returnedJson.results.bindings[i].y.value;
                 }
 
+
                 var myBarChart = new Chart(chartId, {
                     type: 'bar',
                     data: {
                         labels: chartLabels,
                         datasets: [{
-                            label: '',
+                            label: 'Quantity',
                             backgroundColor: 'rgb(23, 125, 255)',
                             borderColor: 'rgb(23, 125, 255)',
                             data: chartData,
@@ -169,6 +170,137 @@ function barchart(element, index) {
     }
 
 }
+
+
+function linechart(element, index) {
+    // get the data that I need
+    // now starts a piece of code that is exactly the same from function counter
+    // ********
+
+    // where I'll store the data necessary fo the bar chart
+    var chartData = [];
+    var chartLabels = [];
+
+    var query = element.query;
+    // check if the query is an API request
+    if (query.startsWith('http')) {
+        alert('There is an API request.');
+        // $.ajax({
+        //     type: 'GET',
+        //     url: query,
+        //     headers: {Accept: 'application/json'},
+        //     success: function (returnedJson) {
+        //         do things
+        //     }
+        // }
+    } else {
+        // if it is a sparql query
+        var encoded = encodeURIComponent(query);
+        var sparqlEndpoint = data.sparql_endpoint;
+        // var label = element.label;
+        $.ajax({
+            type: 'GET',
+            url: sparqlEndpoint + '?query=' + encoded,
+            headers: { Accept: 'application/sparql-results+json; charset=utf-8' },
+            success: function (returnedJson) {
+
+                // first create the HTML structure that'll receive the data
+
+                // create canva for bar chart
+                var chartCanva = document.createElement("canvas");
+                var chartId = "chart" + (index + 1);
+                chartCanva.setAttribute("id", chartId);
+
+                // create div that contains canva
+                var chartArea = document.createElement("div");
+                chartArea.className = "chart-container";
+                chartArea.appendChild(chartCanva);
+
+                // create card body div
+                var cardBody = document.createElement("div");
+                cardBody.className = "card-body";
+                cardBody.appendChild(chartArea);
+
+                // create chart title h3 and add data.label as text
+                var chartTitle = document.createElement("h3");
+                chartTitle.className = "card-title";
+                chartTitle.appendChild(document.createTextNode(element.label));
+
+                // create card header
+                var cardHeader = document.createElement("div");
+                // cardHeader.className = "card-header";
+                cardHeader.appendChild(chartTitle);
+
+                // get general container and append elements
+                var container = document.getElementById("vizContainer");
+                container.appendChild(cardHeader);
+                container.appendChild(cardBody);
+
+                // what I do: check the number of the month and for any missing month assign null as value
+                for (i = 0; i < returnedJson.results.bindings.length; i++) {
+                    chartLabels[i] = returnedJson.results.bindings[i].x.value;
+                    chartData[i] = returnedJson.results.bindings[i].y.value;
+                }
+
+                // graph plotting
+                var myLineChart = new Chart(chartId, {
+                    type: 'line',
+                    data: {
+                        labels: chartLabels,
+                        datasets: [{
+                            label: "New Entries",
+                            borderColor: "#1d7af3",
+                            pointBorderColor: "#FFF",
+                            pointBackgroundColor: "#1d7af3",
+                            pointBorderWidth: 2,
+                            pointHoverRadius: 4,
+                            pointHoverBorderWidth: 1,
+                            pointRadius: 4,
+                            backgroundColor: 'transparent',
+                            fill: true,
+                            borderWidth: 2,
+                            data: chartData
+                        }]
+                    },
+                    options: {
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        spanGaps: true,
+                        legend: {
+                            position: 'bottom',
+                            labels: {
+                                padding: 10,
+                                fontColor: '#1d7af3',
+                            }
+                        },
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        },
+                        tooltips: {
+                            bodySpacing: 4,
+                            mode: "nearest",
+                            intersect: 0,
+                            position: "nearest",
+                            xPadding: 10,
+                            yPadding: 10,
+                            caretPadding: 10
+                        },
+                        layout: {
+                            padding: { left: 15, right: 15, top: 15, bottom: 15 }
+                        }
+                    }
+                });
+            }
+        })
+    }
+
+}
+
+
 
 // function sidebarContent() {
 //     // general container
