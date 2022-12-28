32d1ba5f31a7c435b8bbc4d45166c37e1131404e
marilena <marilena.daquino2@unibo.it>
2022-12-28 15:34:14 +0100

    split components

---
+++ static/js/react/Chart.js
@@ -0,0 +1,20 @@
+const Chart = ({ unique_key, index ,
+                removeComponent , componentList, setComponent,
+                sortComponentUp , sortComponentDown}) => {
+
+    return (
+    <div id="{index}__block_field" className="block_field">
+      <h4 className="block_title">{index}. This is a chart</h4>
+      <SortComponent
+        index={index}
+        sortComponentUp={sortComponentUp}
+        sortComponentDown={sortComponentDown}
+        key={unique_key} />
+      <RemoveComponent
+        index={index}
+        removeComponent={removeComponent}
+        key={unique_key} />
+    </div>
+  );
+
+}
---
+++ static/js/react/Table.js
@@ -0,0 +1,19 @@
+const Table = ({unique_key, index ,
+                removeComponent , componentList, setComponent,
+                sortComponentUp , sortComponentDown}) => {
+    return (
+    <div id="{index}__block_field" className="block_field">
+      <h4 className="block_title">{index}. This is a table</h4>
+      <SortComponent
+        index={index}
+        sortComponentUp={sortComponentUp}
+        sortComponentDown={sortComponentDown}
+        key={unique_key} />
+      <RemoveComponent
+        index={index}
+        removeComponent={removeComponent}
+        key={unique_key} />
+    </div>
+  );
+
+}
