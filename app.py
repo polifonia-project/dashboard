ffe6eff4a12654da6f1cd92f653e353e3a163c74
marilena <marilena.daquino2@unibo.it>
2023-05-30 15:51:28 +0200

    updated urls and config

--- app.py
+++ app.py
@@ -26,10 +26,11 @@ job = scheduler.add_job(utils.empty_temp, 'interval', hours=12)
 scheduler.start()
 
 # paths
-PREFIX = '/melody/'
+PREFIX = '/'
 stories_path = conf.melody_repo_name + '/' + conf.melody_sub_dir
 
-
+@app.route('/')
+@app.route('/melody/')
 @app.route(PREFIX+"")
 @app.route(PREFIX+"index.html")
 def home():
@@ -44,7 +45,7 @@ def asklogin():
 	general_data = data_methods.read_json('config.json')
 	return render_template('asklogin.html', general_data=general_data, stories_path=stories_path)
 
-
+@app.route("/melody/gitauth")
 @app.route(PREFIX+"gitauth")
 def gitauth():
 	"Redirect to github authentication"
@@ -52,6 +53,7 @@ def gitauth():
 	return redirect(github_sync.github_auth+"?client_id="+conf.clientID+scope)
 
 # authenticate user: polifonia, extra
+@app.route("/melody/oauth-callback")
 @app.route(PREFIX+"oauth-callback")
 def oauthcallback(is_valid_user=None):
 	"""Authenticate users via GitHub.
@@ -64,19 +66,20 @@ def oauthcallback(is_valid_user=None):
 	code = request.args.get('code')
 	session["name"],session["user_type"] = github_sync.validate_credentials(code)
 	if session["name"] != 'None':
-		return redirect(url_for('setup'))
+		return redirect('/melody'+url_for('setup'))
 	else:
-		return redirect(url_for('home'))
+		return redirect('/melody'+url_for('home'))
 	print("LOGIN type:", session["user_type"], "| username:", session["name"])
 
-
+@app.route("/melody/signout")
 @app.route(PREFIX+"signout")
 def signout():
 	"Signout and redirect to the homepage."
 	session["name"], session["user_type"] = None , None
-	return redirect(url_for('home'))
+	return redirect('/melody'+url_for('home'))
 
 
+@app.route("/melody/<string:section_name>/<string:datastory_name>", methods=['GET', 'POST'])
 @app.route(PREFIX+"<string:section_name>/<string:datastory_name>", methods=['GET', 'POST'])
 def datastory(section_name, datastory_name):
 	'''
@@ -109,7 +112,7 @@ def datastory(section_name, datastory_name):
 		github_sync.publish_datastory(host,PREFIX,section_name,datastory_name,session)
 		return redirect('https://'+conf.melody_owner+'.github.io/'+conf.melody_repo_name+'/#catalogue')
 
-
+@app.route("/melody/setup", methods=['POST', 'GET'])
 @app.route(PREFIX+"setup", methods=['POST', 'GET'])
 def setup():
 	general_data = data_methods.read_json('config.json')
@@ -126,7 +129,7 @@ def setup():
 				form_data = request.form
 				title, section = data_methods.init_datastory(session['user_type'],
 					form_data,general_data,session['name'])
-				return redirect(url_for("modify_datastory",
+				return redirect('/melody'+url_for("modify_datastory",
 					section_name=section, datastory_name=title))
 			except Exception as e:
 				return str(e)+'did not save to database'
@@ -134,6 +137,7 @@ def setup():
 		return 'something went wrong, try again'
 
 
+@app.route("/melody/modify/<string:section_name>/<string:datastory_name>", methods=['POST', 'GET'])
 @app.route(PREFIX+"modify/<string:section_name>/<string:datastory_name>", methods=['POST', 'GET'])
 def modify_datastory(section_name, datastory_name):
 	while True:
@@ -157,7 +161,7 @@ def modify_datastory(section_name, datastory_name):
 									else 'static/temp/config_'+section_name+'.json'
 								new_datastory_name = data_methods.manage_datastory_data(
 									session['user_type'], general_data, config_file, section_name, datastory_name)
-								return redirect(url_for('datastory',
+								return redirect('/melody'+url_for('datastory',
 										section_name=section_name,
 										datastory_name=new_datastory_name))
 
@@ -166,7 +170,7 @@ def modify_datastory(section_name, datastory_name):
 
 						elif request.form['action'] == 'delete':
 							data_methods.delete_story(general_data,section_name,datastory_name,session['user_type'])
-							return redirect(PREFIX)
+							return redirect(PREFIX+'melody/')
 						else:
 							print('no idea..')
 					except Exception as e:
@@ -212,7 +216,7 @@ def modify_bkg_datastory(section_name, datastory_name):
 
 @app.route(PREFIX+"<string:whatever>/modify/<string:section_name>/<string:datastory_name>", strict_slashes=False, methods=['POST', 'GET'])
 def redirect_to_modify(section_name, datastory_name, whatever=None):
-	return redirect(url_for('modify_datastory', section_name=section_name, datastory_name=datastory_name))
+	return redirect("/melody"+url_for('modify_datastory', section_name=section_name, datastory_name=datastory_name))
 
 
 utils.static_modifications(False)
